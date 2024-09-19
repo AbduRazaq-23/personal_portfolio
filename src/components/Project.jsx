45a64eb827f5d75040a081d18bfca238bc80a511
Abdurazaq333 <irazaqr986@gmail.com>
2024-09-19 14:36:28 +0500

    add project & their picture

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,67 +4,78 @@ import spog from "../assets/project/spogmai.png";
 import youtube from "../assets/project/youtube.jpg";
 import insta from "../assets/project/instagram.jpg";
 import finance from "../assets/project/finance.jpg";
+import goFood from "../assets/project/goFood.png";
+import book from "../assets/project/book.jpg";
 
 const Project = () => {
   const mernStack = [
     {
       id: 1,
+      name: "SpogmaiFoundation",
       img: spog,
       dLink: "https://spogmai-foundation.vercel.app/",
-      cLink: "https://github.com/wasimhaider55/spogmaiFoundation.git",
+      cLink: "https://github.com/wasimhaider55/spogmaiFoundation",
     },
 
     {
       id: 2,
+      name: "E-Commerce",
       img: ecom,
       dLink: "https://e-commerce-frontend-eosin-delta.vercel.app/",
       cLink: "https://github.com/AbduRazaq-23/E-Commerce_Frontend",
+    },
+    {
+      id: 3,
+      name: "FoodDelivery",
+      img: goFood,
+      dLink: "https://github.com/AbduRazaq-23/MERN-goFood",
+      cLink: "https://github.com/AbduRazaq-23/MERN-goFood",
     },
   ];
   const frontend = [
     {
       id: 1,
+      name: "SpogmaiFoundation",
       img: spog,
       dLink: "https://spogmai-foundation.vercel.app/",
       cLink: "https://github.com/wasimhaider55/spogmaiFoundation.git",
     },
     {
       id: 2,
+      name: "E-Commerce",
       img: ecom,
       dLink: "https://e-commerce-frontend-eosin-delta.vercel.app/",
       cLink: "",
-    },
-    {
-      id: 3,
-      img: ecom,
-      dLink: "https://e-commerce-frontend-eosin-delta.vercel.app/",
-      cLink: "",
-    },
-    {
-      id: 4,
-      img: ecom,
-      dLink: "https://e-commerce-frontend-eosin-delta.vercel.app/",
-      cLink: "https://github.com/AbduRazaq-23/E-Commerce_Frontend",
     },
   ];
   const backend = [
     {
       id: 1,
+      name: "YouTube",
       img: youtube,
       dLink: "https://github.com/AbduRazaq-23/YouTubeBackend",
       cLink: "https://github.com/AbduRazaq-23/YouTubeBackend",
     },
     {
       id: 2,
+      name: "Instagram",
       img: insta,
       dLink: "https://github.com/AbduRazaq-23/InstagramBackend",
       cLink: "https://github.com/AbduRazaq-23/InstagramBackend",
     },
     {
       id: 3,
+      name: "PersonalFinanceTracker",
       img: finance,
       dLink: "https://github.com/AbduRazaq-23/personalFinanceTracker",
       cLink: "https://github.com/AbduRazaq-23/personalFinanceTracker",
+    },
+    {
+      id: 4,
+      name: "BookStore",
+      img: book,
+      dLink: "https://github.com/AbduRazaq-23/bookStore",
+      cLink: "https://github.com/AbduRazaq-23/bookStore",
     },
   ];
 
@@ -76,49 +87,21 @@ const Project = () => {
             Projects
           </h1>
         </div>
-        {/* ///////////////////////////// */}
+
         {/* frontend title  */}
         <div className="text-center my-5">
           <h1 className="text-center my-10 font-semibold inline border-b-2 border-pink-600">
             Frontend
           </h1>
         </div>
-        {/* frontend project  */}
         <div className="w-full p-3 lg:p-0 grid grid-cols-2 md:grid-cols-4  justify-center gap-2 mb-10">
           {frontend.map((d) => (
             <div
               key={d.id}
-              className=" shadow-2xl shadow-[#040c16] rounded-md overflow-hidden hover:scale-105 duration-500 h-60"
+              className=" shadow-2xl shadow-[#040c16] rounded-md overflow-hidden hover:scale-105 duration-500 "
             >
-              <img className="h-[65%] w-full" src={d.img} alt="img" />
-              <a href={d.dLink} target="_blank">
-                <button className="w-full   bg-[#344853] my-1 py-2 text-sm font-semibold hover:font-bold">
-                  Demo
-                </button>
-              </a>
-              <a href={d.cLink} target="_blank">
-                <button className="w-full   bg-[#344853] py-2  text-sm font-semibold hover:font-bold">
-                  Code
-                </button>
-              </a>
-            </div>
-          ))}
-        </div>
-        {/* //////////////////////////////////////////// */}
-        {/* backend title  */}
-        <div className="text-center my-5">
-          <h1 className="text-center my-10 font-semibold inline border-b-2 border-pink-600">
-            Backend
-          </h1>
-        </div>
-        {/* backend project  */}
-        <div className="w-full p-3 lg:p-0 grid grid-cols-2 md:grid-cols-4  justify-center gap-2 mb-10">
-          {backend.map((d) => (
-            <div
-              key={d.id}
-              className=" shadow-2xl shadow-[#040c16] rounded-md overflow-hidden hover:scale-105 duration-500 h-60"
-            >
-              <img className="h-[65%] w-full" src={d.img} alt="img" />
+              <h1 className="text-center font-semibold">{d.name}</h1>
+              <img className="h-44 w-full rounded-md" src={d.img} alt="img" />
               <a href={d.dLink} target="_blank">
                 <button className="w-full   bg-[#344853] my-1 py-2 text-sm font-semibold hover:font-bold">
                   Demo
@@ -133,21 +116,48 @@ const Project = () => {
           ))}
         </div>
 
-        {/* /////////////////////////////////////// */}
-        {/* mern title  */}
+        {/* backend   */}
+        <div className="text-center my-5">
+          <h1 className="text-center my-10 font-semibold inline border-b-2 border-pink-600">
+            Backend
+          </h1>
+        </div>
+        <div className="w-full p-3 lg:p-0 grid grid-cols-2 md:grid-cols-4  justify-center gap-2 mb-10">
+          {backend.map((d) => (
+            <div
+              key={d.id}
+              className=" shadow-2xl shadow-[#040c16] rounded-md overflow-hidden hover:scale-105 duration-500 "
+            >
+              <h1 className="text-center font-semibold">{d.name}</h1>
+              <img className="h-44 w-full rounded-md" src={d.img} alt="img" />
+              {/* <a href={d.dLink} target="_blank">
+                <button className="w-full   bg-[#344853] my-1 py-2 text-sm font-semibold hover:font-bold">
+                  Demo
+                </button>
+              </a> */}
+              <a href={d.cLink} target="_blank">
+                <button className="w-full   bg-[#344853] py-2  text-sm font-semibold hover:font-bold">
+                  Code
+                </button>
+              </a>
+            </div>
+          ))}
+        </div>
+
+        {/* mern stack  */}
         <div className="text-center my-5">
           <h1 className="text-center my-10 font-semibold inline border-b-2 border-pink-600">
             MERN Stack
           </h1>
         </div>
-        {/* MERN STACK PROJECT  */}
         <div className="w-full p-3 lg:p-0 grid grid-cols-2 md:grid-cols-4  justify-center gap-2">
           {mernStack.map((d) => (
             <div
               key={d.id}
-              className=" shadow-2xl shadow-[#040c16] rounded-md overflow-hidden hover:scale-105 duration-500 h-60"
+              className=" shadow-2xl shadow-[#040c16] rounded-md overflow-hidden hover:scale-105 duration-500 "
             >
-              <img className="h-[65%] w-full" src={d.img} alt="img" />
+              <h1 className="text-center font-semibold">{d.name}</h1>
+              <img className="h-44 w-full rounded-md" src={d.img} alt="img" />
               <a href={d.dLink} target="_blank">
                 <button className="w-full   bg-[#344853] my-1 py-2 text-sm font-semibold hover:font-bold">
                   Demo
@@ -161,7 +171,6 @@ const Project = () => {
             </div>
           ))}
         </div>
-        {/* ///////////////////////////////// */}
       </div>
     </div>
   );
