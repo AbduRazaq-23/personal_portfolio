d6e2609e7d8015ab7a0b66fc6762f0f9b6dfc53e
Abdurazaq333 <irazaqr986@gmail.com>
2024-09-24 14:29:43 +0500

    changes in form ui

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -6,39 +6,35 @@ const ContactUs = () => {
       name="contact"
       className="w-full  bg-[#2e424d] flex justify-center items-center py-20"
     >
-      <form
-        method="Email"
-        action="irazaqr987@gmail.com"
-        className="flex flex-col max-w-[600px] w-full"
-      >
+      <form className="flex flex-col max-w-[600px] w-full text-gray-300 font-semibold">
         <div className="pb- text-center">
-          <p className="text-2xl font-semibold inline border-b-4 border-[#98dad9] text-gray-300">
+          <p className="text-2xl  inline border-b-4 border-[#98dad9] ">
             Contact
           </p>
-          <p className="text-gray-300 text-sm font-semibold py-4">
+          <p className=" text-sm py-4">
             Submit the form below or shoot me an email -
             abdurazaq.dev23@gmail.com
           </p>
         </div>
         <input
-          className="bg-[#ccd6f6] p-2"
+          className="bg-[#2e424d] text-sm border border-gray-300 p-2 rounded-md m-1 focus:outline-none"
           type="text"
           placeholder="Name"
           name="name"
         />
         <input
-          className="my-4 p-2 bg-[#ccd6f6]"
+          className="bg-[#2e424d] text-sm  border border-gray-300 p-2 rounded-md m-1 focus:outline-none"
           type="email"
           placeholder="Email"
           name="email"
         />
         <textarea
-          className="bg-[#ccd6f6] p-2"
+          className="bg-[#2e424d] text-sm  border border-gray-300 p-2 rounded-md m-1 focus:outline-none"
           name="message"
           rows="10"
           placeholder="Message"
         ></textarea>
-        <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
+        <button className=" border border-gray-300 rounded-md hover:text-[#2e424d] hover:bg-[#98dad9] hover:border-gray-300 px-4 py-1  m-1 ">
           Let's Send
         </button>
       </form>
