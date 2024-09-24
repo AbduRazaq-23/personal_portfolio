f8a73cd920e8d2fc3b2b8372648c1ddafa198785
Abdurazaq333 <irazaqr986@gmail.com>
2024-09-24 14:43:46 +0500

    nav menu change

--- src/components/nav/NavMenu.js
+++ src/components/nav/NavMenu.js
@@ -17,11 +17,11 @@ export const Menu = [
   {
     id: "4",
     path: "about",
-    title: "AboutUs",
+    title: "About",
   },
   {
     id: "5",
     path: "contact",
-    title: "ContactUs",
+    title: "Contact",
   },
 ];
