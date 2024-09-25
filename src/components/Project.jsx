af87a3e19b63d199b0c9a9e9b3d947cae9bd3c82
Abdurazaq333 <irazaqr986@gmail.com>
2024-09-25 17:27:02 +0500

    update frontend project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -89,7 +89,7 @@ const Project = () => {
 
   return (
     <div name="experience" className=" w-full py-20 bg-[#2e424d] text-gray-300">
-      <div className="max-w-[1000px] mx-auto">
+      <div className="max-w-[1000px] mx-auto justify-center">
         <div className="text-center mb-10">
           <h1 className="text-2xl font-semibold inline border-b-4 border-[#98dad9]">
             Projects
@@ -97,26 +97,27 @@ const Project = () => {
         </div>
 
         {/* frontend title  */}
+
         <div className="text-center my-5">
           <h1 className="text-center my-10 font-bold inline text-[#98dad9]">
             Frontend
           </h1>
         </div>
-        <div className="w-full p-3 lg:p-0 grid grid-cols-2 md:grid-cols-4  justify-center gap-2 mb-10">
+        <div className="p-3 lg:p-0 grid grid-cols-2 md:grid-cols-4 gap-2 mb-10 justify-center mx-auto">
           {frontend.map((d) => (
             <div
               key={d.id}
-              className=" shadow-2xl shadow-[#040c16] rounded-md overflow-hidden hover:scale-105 duration-500 "
+              className="shadow-2xl shadow-[#040c16] rounded-md overflow-hidden hover:scale-105 duration-500"
             >
               <h1 className="text-center font-semibold">{d.name}</h1>
               <img className="h-44 w-full rounded-md" src={d.img} alt="img" />
-              <a href={d.dLink} target="_blank">
-                <button className="w-full   bg-[#344853] my-1 py-2 text-sm font-semibold hover:font-bold">
+              <a href={d.dLink} target="_blank" rel="noopener noreferrer">
+                <button className="w-full bg-[#344853] my-1 py-2 text-sm font-semibold hover:font-bold">
                   Demo
                 </button>
               </a>
-              <a href={d.cLink} target="_blank">
-                <button className="w-full   bg-[#344853] py-2  text-sm font-semibold hover:font-bold">
+              <a href={d.cLink} target="_blank" rel="noopener noreferrer">
+                <button className="w-full bg-[#344853] py-2 text-sm font-semibold hover:font-bold">
                   Code
                 </button>
               </a>
@@ -138,11 +139,7 @@ const Project = () => {
             >
               <h1 className="text-center font-semibold">{d.name}</h1>
               <img className="h-44 w-full rounded-md" src={d.img} alt="img" />
-              {/* <a href={d.dLink} target="_blank">
-                <button className="w-full   bg-[#344853] my-1 py-2 text-sm font-semibold hover:font-bold">
-                  Demo
-                </button>
-              </a> */}
+
               <a href={d.cLink} target="_blank">
                 <button className="w-full   bg-[#344853] py-2  text-sm font-semibold hover:font-bold">
                   Code
