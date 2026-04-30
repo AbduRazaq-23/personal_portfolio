55313ef38d5098e42c0fd06825ecd5d8350c82c5
Abdurazaq333 <irazaqr986@gmail.com>
2026-04-30 12:29:22 +0500

    fix image isssue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,38 +8,36 @@ const Home = () => {
       name="home"
       className="w-full h-[80%] md:h-[80%] pb-10 bg-[#051119] mx-auto pt-20"
     >
-      {pic && (
-        <div className="grid md:grid-cols-2 max-w-[80%] m-auto">
-          <motion.div
-            initial={{ opacity: 0, scale: 0.9 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.9, ease: "easeOut" }}
-            className="flex flex-col justify-center items-center md:items-start w-full px-2 py-8"
-          >
-            <p className=" text-[#98dad9] font-bold xs:pt-10">Hi, My Name is</p>
-            <h1 className="py-3 text-3xl md:text-5xl font-bold text-[#eaebed]">
-              ABDU RAZAQ
-            </h1>
-            <p className="text-center md:text-left md:text-2xl text-[#eaebed]">
-              Software Engineer & MERN Stack Developer
-            </p>
-          </motion.div>
+      <div className="grid md:grid-cols-2 max-w-[80%] m-auto">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.9, ease: "easeOut" }}
+          className="flex flex-col justify-center items-center md:items-start w-full px-2 py-8"
+        >
+          <p className=" text-[#98dad9] font-bold xs:pt-10">Hi, My Name is</p>
+          <h1 className="py-3 text-3xl md:text-5xl font-bold text-[#eaebed]">
+            ABDU RAZAQ
+          </h1>
+          <p className="text-center md:text-left md:text-2xl text-[#eaebed]">
+            Software Engineer & MERN Stack Developer
+          </p>
+        </motion.div>
 
-          <motion.div
-            initial={{ opacity: 0, y: 40 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8, ease: "easeOut" }}
-          >
-            <div className=" w-[60%] md:h-[80%]  mx-auto bg-[#134242]  rotate-3 py-5 rounded-xl md:mt-14 h-100 relative ">
-              <img
-                className=" w-full h-full object-cover  rounded-xl -rotate-3"
-                src={pic}
-                alt="abdurazaq"
-              />
-            </div>
-          </motion.div>
-        </div>
-      )}
+        <motion.div
+          initial={{ opacity: 0, y: 40 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+        >
+          <div className=" w-[60%] md:h-[80%]  mx-auto bg-[#134242]  rotate-3 py-5 rounded-xl md:mt-14 h-100 relative ">
+            <img
+              className=" w-full h-full object-cover  rounded-xl -rotate-3"
+              src={pic}
+              alt="abdurazaq"
+            />
+          </div>
+        </motion.div>
+      </div>
     </div>
   );
 };
